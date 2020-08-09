9bba4504729bf5aecc71f176366004cd3e4aecbc
Pankaj garg <gargpankaj901@gmail.com>
2020-08-09 21:28:07 +0530

    redis implementation

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,12 @@
 module.exports = {
+  // googleClientID:
+  //   '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  // googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  // cookieKey: '123123123',
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "13692393153-ink61j259cqr1e3s4ol8boidgsshohdt.apps.googleusercontent.com",
+  googleClientSecret: "6VHCgwPwspFw3Xm6Jre2kbae",
+  mongoURI: "mongodb://mydatabase:password@ds143559.mlab.com:43559/emaily-dev",
+  cookieKey: "thisisforencryption"
 };
